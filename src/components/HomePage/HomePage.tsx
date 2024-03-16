402c98b5f28b55dcb01a98fe9362521aea4691db
Ivanna Zaiets <iva.zaiets@gmail.com>
2024-03-16 13:40:50 +0200

    changes

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,7 +1,10 @@
 const HomePage = () => {
-    return (
-        <h1>Heloo this is HomePage</h1>
-    )
-}
+  return (
+    <>
+      <h1>Heloo this is HomePage</h1>
+      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fscience.nasa.gov%2Fsun%2F&psig=AOvVaw12ERBE4Xv5QFPet5llnmLf&ust=1710675605702000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCJD_0q_Z-IQDFQAAAAAdAAAAABAE" alt="" />
+    </>
+  );
+};
 
 export default HomePage;
