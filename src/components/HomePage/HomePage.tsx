60a148630331afb4509e590855bdb730b0811e9f
Ivanna Zaiets <iva.zaiets@gmail.com>
2024-03-16 13:47:48 +0200

    changes

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -2,7 +2,16 @@ const HomePage = () => {
   return (
     <>
       <h1>Heloo this is HomePage</h1>
-      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fscience.nasa.gov%2Fsun%2F&psig=AOvVaw12ERBE4Xv5QFPet5llnmLf&ust=1710675605702000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCJD_0q_Z-IQDFQAAAAAdAAAAABAE" alt="" />
+      <img
+        style={{
+          width: "50px",
+          height: "50px",
+          border: "none",
+          borderRadius: "50%",
+        }}
+        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fscience.nasa.gov%2Fsun%2F&psig=AOvVaw12ERBE4Xv5QFPet5llnmLf&ust=1710675605702000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCJD_0q_Z-IQDFQAAAAAdAAAAABAE"
+        alt=""
+      />
     </>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/exmpl/"
+  base: "/exmpl/",
+  build: {
+    outDir: 'build',
+  },
 })
